af9a3c66a2f3df4c514e79cddbd55d1f44c67ede
JeanPaulo21 <126036883+JeanPaulo21@users.noreply.github.com>
2024-12-17 19:20:04 +0000

    diseño login

--- pages/login.php
+++ pages/login.php
@@ -1,19 +1,43 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <title>Iniciar Sesión</title>
-</head>
-<body>
     <?php include '../includes/header.php'; ?>
-    <h2>Iniciar Sesión</h2>
-    <form action="../login_process.php" method="POST">
-        <label for="username">Usuario:</label>
-        <input type="text" name="username" required><br>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
 
-        <label for="password">Contraseña:</label>
-        <input type="password" name="password" required><br>
+<body>
+    <div class="login-container">
+        <h2>Iniciar Sesión</h2>
+        <form action="../login_process.php" method="POST">
+            <div class="mb-3">
+                <label for="username" class="form-label">Usuario</label>
+                <input type="text" class="form-control" id="username" name="username" placeholder="Ingrese su usuario" required>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Contraseña</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Ingrese su contraseña" required>
+            </div>
+            <div class="d-grid">
+                <button type="submit" class="btn btn-login">Iniciar Sesión</button>
+            </div>
+        </form>
+    </div>
 
-        <button type="submit">Iniciar Sesión</button>
-    </form>
+    <footer class="footer">
+        <h2>Contáctanos</h2>
+        <h4>+506 8745 2819</h4>
+        <div style="margin: 10px 0;">
+            <i class="fa-brands fa-facebook fa-xl icon-facebook"></i>
+            <i class="fa-brands fa-instagram fa-xl icon-instagram"></i>
+            <i class="fa-brands fa-x-twitter fa-xl icon-twitter"></i>
+            <i class="fa-brands fa-whatsapp fa-xl icon-whatsapp"></i>
+
+        </div>
+        <p class="derechos">© Derechos reservados</p>
+    </footer>
+
 </body>
+
 </html>
